d2f31752e275d13db6843fe4a22af9ce22de1704
kml_c_17 <kocenkotars@gmail.com>
2017-02-23 13:06:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*public class Main {
+
+    public static void main(String[] args) throws IOException {
+      *//*  Scanner scanner = new Scanner(System.in);
+        String []s = scanner.nextLine().split("\\s");
+        System.out.println(Arrays.toString(s));*//*
+      */
+public class Main{
+    public static void main(String[] args) {
+        int a=7;
+        int b=2;
+        System.out.println(a/b);
+
+    }
+    }
